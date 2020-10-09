0bce30c439f7fd7fdf89b7d472c72075684ead96
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2020-10-09 23:54:02 +0200

    Removed Player module and wrote the amount of player prompt

--- src/app.js
+++ src/app.js
@@ -6,12 +6,30 @@
  * @author Sebastian Åkerblom <sa224ny@student.lnu.se>
  * @version 1.0.0
  */
+// Imports
+import { Deck } from './Deck.js'
+// From https://nodejs.org/api/readline.html
+import readline from 'readline'
+// Readline interface?
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-// TODO: Replace the code below with your own game logic.
-import * as player from './Player.js'
+// Create a deck of 52 cards
+const playingCards = Deck.create()
+// Shuffle the deck
+Deck.shuffle(playingCards)
+
+// Reduce function to be used multiple times
+const reducer = (a, b) => a + b
 
 const main = function () {
-  console.log(player)
+  rl.question('How many players? ', (answer) => {
+    console.log(`Number of players will be ${answer}`)
+
+    rl.close()
+  })
 }
 
 main()
